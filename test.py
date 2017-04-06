f3309e040be29a4486cbf085ed7b6018a56c57ee
haoziiy <joker.xieyuri@qq.com>
2017-04-06 18:53:07 +0800

    生成头像

---
+++ test.py
@@ -0,0 +1,3 @@
+import avatar_generator
+img= avatar_generator.render_identicon('123123', 16)
+img.show()
---
+++ test_batch.py
@@ -0,0 +1,16 @@
+import avatar_generator
+def gen_avatar_batch(code,size):
+	img= avatar_generator.render_identicon(code, 16)
+	#img.show()
+	img.save('%s_%s.png'%(code,size))
+
+for x in xrange(10000000,10000000+5):
+    gen_avatar_batch(x, 16)
+for x in xrange(20000000,20000000+5):
+    gen_avatar_batch(x, 16)
+for x in xrange(40000000,40000000+5):
+    gen_avatar_batch(x, 16)
+for x in xrange(80000000,80000000+5):
+    gen_avatar_batch(x, 16)
+for x in xrange(160000000,160000000+5):
+    gen_avatar_batch(x, 16)
